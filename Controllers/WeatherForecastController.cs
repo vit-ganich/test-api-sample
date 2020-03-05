86f8463774c66e621f630e11c1f092366ebe4708
Vitali Hanich <vitali.hanich@gmail,com>
2020-03-05 23:07:01 +0300

    updates

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace APIsample.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("WeatherForecast")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -9,6 +9,7 @@ namespace APIsample.Models
     {
         public long Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public bool IsComplete { get; set; }
     }
 }
